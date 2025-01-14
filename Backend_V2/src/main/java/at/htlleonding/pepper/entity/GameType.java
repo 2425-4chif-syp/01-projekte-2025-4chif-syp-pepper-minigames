be432b2e83246c2a172563d372dc19de6fa2459e
htl-leonding <t.stuetz@htl-leonding.ac.at>
2025-01-14 21:42:56 +0100

    add constructors, getter and setter to GameType

--- Backend_V2/src/main/java/at/htlleonding/pepper/entity/GameType.java
+++ Backend_V2/src/main/java/at/htlleonding/pepper/entity/GameType.java
@@ -6,10 +6,46 @@ import jakarta.persistence.*;
 @Table(name="pe_game_type")
 public class GameType {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "gt_id")
     private Long id;
 
     @Column(name = "gt_name")
     private String name;
+
+    //region Constructors
+    public GameType() {
+    }
+
+    public GameType(String name) {
+        this.name = name;
+    }
+    //endregion
+
+    //region getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return "GameType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
