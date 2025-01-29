3c83e79ff9cdc2278008629c85b77cb413f75a50
Obaid <mushtaqobaidullah@gmail.com>
2025-01-29 09:45:52 +0100

    Implemented BootReceiver to launch MainActivity after device boot

--- PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/receivers/BootReceiver.kt
+++ PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/receivers/BootReceiver.kt
@@ -3,9 +3,16 @@ package com.example.menu.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import com.example.menu.MainActivity
 
 class BootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        // Placeholder - wird später implementiert
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d("BootReceiver", "BOOT_COMPLETED received!")
+            val i = Intent(context, MainActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(i)
+        }
     }
 }
