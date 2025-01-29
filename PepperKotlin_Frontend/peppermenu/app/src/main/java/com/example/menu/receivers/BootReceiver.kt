67315b525bf75b14d67737ae6dccaf3955988b3a
Obaid <mushtaqobaidullah@gmail.com>
2025-01-29 09:43:05 +0100

    Added BootReceiver placeholder class for handling BOOT_COMPLETED intent

---
+++ PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/receivers/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.menu.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // Placeholder - wird später implementiert
+    }
+}
