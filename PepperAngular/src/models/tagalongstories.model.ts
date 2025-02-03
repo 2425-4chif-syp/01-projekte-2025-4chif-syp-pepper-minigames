0a871f9cccf0ae6056db3aa696600e48e031bec2
Kyaw <kyawhtethtun65@gmail.com>
2025-02-03 01:45:41 +0100

    Added 2 new Interfaces: IMove and IImage, Added a MoveHandler class that convert via move name to an IMove interface.

--- PepperAngular/src/models/tagalongstories.model.ts
+++ PepperAngular/src/models/tagalongstories.model.ts
@@ -9,14 +9,82 @@ export interface ITagalongStory {
 export interface IStep {
   id?: number;
   duration: number;
-  moveNameAndDuration?: string | null;
+  move?: string | IMove;
   text: string;
-  image: string;
+  image: string | IImage;
   index: number;
-  tagAlongStoryId?: number | null;
+  game?: number | null;
 }
 
 export interface IGameType{
   id: string;
   name: string;
+}
+
+export interface IMove{
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface IImage{
+  person: string;
+  image: string;
+  url: string;
+  description: string;
+}
+
+export class MoveHandler {
+  // Define private properties dynamically
+  private movesMap: { [key: string]: IMove } = {};
+
+  public moves = [
+    'emote_hurra',
+    'essen',
+    'gehen',
+    'hand_heben',
+    'highfive_links',
+    'highfive_rechts',
+    'klatschen',
+    'strecken',
+    'umher_sehen',
+    'winken',
+  ];
+
+  public moveNames = [
+    'Hurra',
+    'Essen',
+    'Gehen',
+    'Hand heben',
+    'Highfive links',
+    'Highfive rechts',
+    'Klatschen',
+    'Strecken',
+    'Umher sehen',
+    'Winken',
+  ];
+
+  constructor() {
+    this.generateMoves();
+  }
+
+  private generateMoves(): void {
+    this.moves.forEach((move, index) => {
+      const moveName = this.moveNames[index];
+      this.movesMap[move] = {
+        id: index + 1, // Assign unique ID (starting from 1)
+        name: move,
+        description: moveName,
+      };
+    });
+  }
+
+  // Method to retrieve a move by name
+  public getMove(name: string): IMove | undefined {
+    return this.movesMap[name];
+  }
+}
+
+export class StepHandler {
+  
 }
