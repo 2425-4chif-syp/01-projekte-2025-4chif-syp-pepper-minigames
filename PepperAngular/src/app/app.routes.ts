aaeb389962925ac99cbcb66a6b008da7013eade7
Kyaw <kyawhtethtun65@gmail.com>
2025-02-03 02:00:22 +0100

    Created a new route "addstep" so it seperates creating the cover for the Tag Along Story and the scene aka. steps for the story. The reason behind the seperation is to extract database access and abstract the code.

--- PepperAngular/src/app/app.routes.ts
+++ PepperAngular/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { MemoryComponent } from './memory/memory.component';
 import { CreatestoryComponent } from './createstory/createstory.component';
 import { EditstoryComponent } from './editstory/editstory.component';
+import { AddstepComponent } from './addstep/addstep.component';
 
 export const routes: Routes = [
   {path: 'tagalongstory', component: TagalongstoryComponent},
   {path: '', component: HomePageComponent},
   {path: 'memory', component: MemoryComponent},
   {path: 'createstory', component: CreatestoryComponent},
+  {path: 'addstep/:id', component: AddstepComponent},
   {path: 'editstory/:id', component: EditstoryComponent}
 ];
