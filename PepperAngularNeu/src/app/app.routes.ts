4fea045d8919eccb1aa7003830054898bd8e122a
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-02-27 13:28:09 +0100

    fix: Unser Angular neubenannt

---
+++ PepperAngularNeu/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { TagalongstoryComponent } from './tagalongstory/tagalongstory.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { EditstoryComponent } from './editstory/editstory.component';
+import { AddstepComponent } from './addstep/addstep.component';
+import { PersonEntryComponent } from './person-entry/person-entry.component';
+
+export const routes: Routes = [
+  {path: 'tagalongstory', component: TagalongstoryComponent},
+  {path: '', component: HomePageComponent},
+  {path: 'addstep/:id', component: AddstepComponent},
+  {path: 'editstory/:id', component: EditstoryComponent},
+  {path: 'person-entry', component: PersonEntryComponent}
+];
+
---
+++ PepperAngularNeu/src/app/person-entry/person-entry.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-person-entry',
+    templateUrl: './person-entry.component.html',
+    styleUrls: ['./person-entry.component.css'],
+    standalone: false
+})
+export class PersonEntryComponent {
+  person = {
+    firstName: '',
+    lastName: '',
+    roomNumber: ''
+  };
+
+  updatePerson(field: keyof typeof this.person, event: Event) {
+    const inputElement = event.target as HTMLInputElement | null;
+    if (inputElement && inputElement.value !== null) {
+      this.person[field] = inputElement.value;
+    }
+  }
+
+  onLogin(event: Event) {
+    event.preventDefault(); // Verhindert Standard-Seiten-Neuladen
+    console.log("Person eingeloggt:", this.person);
+    alert(`Willkommen ${this.person.firstName} ${this.person.lastName}, Raum: ${this.person.roomNumber}`);
+  }
+}
