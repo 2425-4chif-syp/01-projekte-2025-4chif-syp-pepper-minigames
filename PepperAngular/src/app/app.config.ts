af9b64493591a80a78c0eb2ac552c2498d501663
htl-leonding <t.stuetz@htl-leonding.ac.at>
2025-02-27 21:34:01 +0100

    ci: add environment for dev- and prod-configuration

--- PepperAngular/src/app/app.config.ts
+++ PepperAngular/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import {environment} from "../environments/environment";
 
 export const STORY_URL= new InjectionToken<string>('STORY_URL');
 
@@ -10,7 +11,10 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withComponentInputBinding()),
     {
       provide: STORY_URL,
-      useValue: 'http://152.67.78.190:8080/tagalongstories'
+      // useValue: 'http://152.67.78.190:8080/tagalongstories'
+      //useValue: 'http://152.67.78.190:8080/tagalongstories'
+      // for using the production url -> ng build --configuration=production
+      useValue: environment.apiUrl
     },
     provideHttpClient(),]
 };
