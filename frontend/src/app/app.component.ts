e7ec048d4731f4fa2223dfc6bbb711a8386d93c6
BrunnerMoritz <m.brunner@students.htl-leonding.ac.at>
2025-03-06 11:37:13 +0100

    fix: bug fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
