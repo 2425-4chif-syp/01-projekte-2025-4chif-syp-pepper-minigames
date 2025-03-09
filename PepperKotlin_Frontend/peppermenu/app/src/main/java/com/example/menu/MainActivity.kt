10ccf28a158c2b534ab79afbb524b7ca5b785e79
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-03-09 21:32:40 +0100

    fix: MainActivity zu einer RoboterActivity umgewandelt #PEP-80

--- PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/MainActivity.kt
+++ PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.menu
 
 import MainMenuScreen
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.menu.navigation.AppNavigation
 import com.example.menu.presentation.LoginScreen
 import com.example.menu.ui.theme.MenuTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), RobotLifecycleCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        QiSDK.register(this, this)
+
         setContent {
             MenuTheme {
                 // Initialisiere NavController
@@ -52,5 +56,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRobotFocusLost() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRobotFocusRefused(reason: String?) {
+        TODO("Not yet implemented")
     }
 }
