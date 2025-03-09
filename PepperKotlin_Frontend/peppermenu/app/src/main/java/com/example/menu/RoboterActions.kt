64a6d765999523a0f500d1a56509d3f6d15a0a15
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-03-09 22:50:05 +0100

    fix: Fotofunktion verbessert  #PEP-81

--- PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/RoboterActions.kt
+++ PepperKotlin_Frontend/peppermenu/app/src/main/java/com/example/menu/RoboterActions.kt
@@ -1,8 +1,19 @@
 package com.example.menu
 
+import android.content.ContentValues
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.`object`.camera.TakePicture
 import com.aldebaran.qi.sdk.`object`.conversation.Say
+import com.aldebaran.qi.sdk.`object`.image.EncodedImage
+import com.aldebaran.qi.sdk.`object`.image.EncodedImageHandle
 import com.aldebaran.qi.sdk.builder.SayBuilder
+import com.aldebaran.qi.sdk.builder.TakePictureBuilder
+import com.example.menu.network.ApiHelper
+import java.nio.ByteBuffer
 import java.util.concurrent.Future
 
 class RoboterActions {
@@ -31,5 +42,34 @@ class RoboterActions {
             }
             return null;
         }
+
+        fun takePicture(onImageCaptured: (ImageBitmap) -> Unit){
+            if (robotExecute) {
+                val takePictureFuture: com.aldebaran.qi.Future<TakePicture>? = TakePictureBuilder.with(qiContext).buildAsync()
+
+                val timestampedImageHandleFuture = takePictureFuture?.andThenCompose { takePicture ->
+                    Log.i(ContentValues.TAG, "Take picture launched!")
+                    takePicture.async().run()
+                }
+
+                timestampedImageHandleFuture?.andThenConsume { timestampedImageHandle ->
+                    Log.i(ContentValues.TAG, "Picture taken")
+                    val encodedImageHandle: EncodedImageHandle = timestampedImageHandle.image
+                    val encodedImage: EncodedImage = encodedImageHandle.value
+
+                    val buffer: ByteBuffer = encodedImage.data
+                    buffer.rewind()
+                    val pictureArray = ByteArray(buffer.remaining())
+                    buffer.get(pictureArray)
+
+                    val pictureBitmap = BitmapFactory.decodeByteArray(pictureArray, 0, pictureArray.size)
+                    val pictureImageBitmap = pictureBitmap.asImageBitmap()
+                    Log.i(ContentValues.TAG, "Picture: ${pictureImageBitmap}")
+
+                    onImageCaptured(pictureImageBitmap)
+                }
+            }
+        }
+
     }
 }
