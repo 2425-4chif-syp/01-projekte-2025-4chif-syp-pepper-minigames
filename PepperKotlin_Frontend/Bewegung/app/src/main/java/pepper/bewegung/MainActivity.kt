e7868ab87f3fe93cb189a3f935714238952e6feb
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-03-12 14:21:59 +0100

    feat: Applikation ist jetzt ein Pepper Roboter tauglich #PEP-87

--- PepperKotlin_Frontend/Bewegung/app/src/main/java/pepper/bewegung/MainActivity.kt
+++ PepperKotlin_Frontend/Bewegung/app/src/main/java/pepper/bewegung/MainActivity.kt
@@ -1,8 +1,7 @@
-package pepper.bewegung
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,34 +9,34 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import pepper.bewegung.ui.theme.BewegungTheme
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), RobotLifecycleCallbacks {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BewegungTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this)
+        setContent{
+            Column() {
+                Text("Hello Pepper")
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BewegungTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+    override fun onRobotFocusGained(qiContext: QiContext) {
+        // The robot focus is gained.
+    }
+    override fun onRobotFocusLost() {
+        // The robot focus is lost.
+    }
+    override fun onRobotFocusRefused(reason: String) {
+        // The robot focus is refused.
     }
 }
