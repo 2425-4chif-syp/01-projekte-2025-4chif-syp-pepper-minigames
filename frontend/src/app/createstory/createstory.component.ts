9ab6ec53d25bc1645f1ec6fea1f82f5ad05df5ba
BrunnerMoritz <m.brunner@students.htl-leonding.ac.at>
2025-03-12 15:33:10 +0100

    feat: movements and duration is available now

--- frontend/src/app/createstory/createstory.component.ts
+++ frontend/src/app/createstory/createstory.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 
-
 interface Scene {
   speech: string;
   movement: string;
@@ -10,12 +9,38 @@ interface Scene {
   image: string;
 }
 
-@Component({
+@Component({  
   selector: 'app-createstory',
   imports: [DragDropModule, CommonModule],
   templateUrl: './createstory.component.html',
 })
 export class CreatestoryComponent {
+  public duration = [5, 10, 15];
+  public moves = [
+    'emote_hurra',
+    'essen',
+    'gehen',
+    'hand_heben',
+    'highfive_links',
+    'highfive_rechts',
+    'klatschen',
+    'strecken',
+    'umher_sehen',
+    'winken',
+  ];
+  public moveNames = [
+    'Hurra',
+    'Essen',
+    'Gehen',
+    'Hand heben',
+    'Highfive links',
+    'Highfive rechts',
+    'Klatschen',
+    'Strecken',
+    'Umher sehen',
+    'Winken',
+  ];
+  
   scenes: Scene[] = [
     {
       speech: 'Während die Zirkusvorstellung beginnt, winken wir begeistert den talentierten Artisten zu...',
@@ -33,5 +58,15 @@ export class CreatestoryComponent {
 
   drop(event: CdkDragDrop<Scene[]>) {
     moveItemInArray(this.scenes, event.previousIndex, event.currentIndex);
+  }
+
+  updateMovement(event: Event, scene: Scene) {
+    const selectElement = event.target as HTMLSelectElement;
+    scene.movement = selectElement.value;
+  }
+
+  updateDuration(event: Event, scene: Scene) {
+    const selectElement = event.target as HTMLSelectElement;
+    scene.duration = Number(selectElement.value);
   }
 }
