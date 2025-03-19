4266391232dc6aafeeebab2ef6d85e1d3d9c1091
obaidullah-Mushtaq <o.mushtaq@students.htl-leonding.ac.at>
2025-03-19 10:14:44 +0100

    Created new Component

---
+++ frontend/src/app/move-selector/move-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-move-selector',
+  imports: [],
+  templateUrl: './move-selector.component.html',
+  styleUrl: './move-selector.component.css'
+})
+export class MoveSelectorComponent {
+
+}
