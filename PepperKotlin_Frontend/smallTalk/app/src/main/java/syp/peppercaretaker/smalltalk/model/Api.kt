6c98b3f49d3f4ad1689bedafcb26ed2ae26af68e
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-04-27 22:12:49 +0200

    fix: Timeout von 90 auf 20 Sekunden geändert #PEP-116

--- PepperKotlin_Frontend/smallTalk/app/src/main/java/syp/peppercaretaker/smalltalk/model/Api.kt
+++ PepperKotlin_Frontend/smallTalk/app/src/main/java/syp/peppercaretaker/smalltalk/model/Api.kt
@@ -19,8 +19,8 @@ class Api {
 
         // Client-Objekt einmal erstellen und wiederverwenden
         private val client = OkHttpClient.Builder()
-            .connectTimeout(90, TimeUnit.SECONDS)
-            .readTimeout(90, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
             .protocols(listOf(Protocol.HTTP_1_1))
             .build()
 
