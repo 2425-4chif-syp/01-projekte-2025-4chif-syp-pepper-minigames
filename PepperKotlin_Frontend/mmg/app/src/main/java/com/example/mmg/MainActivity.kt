459130cce03400e86f5233f5ae181f24e2dd8da7
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-09-22 08:56:04 +0200

    fix: viewmodel as activity-scope

--- PepperKotlin_Frontend/mmg/app/src/main/java/com/example/mmg/MainActivity.kt
+++ PepperKotlin_Frontend/mmg/app/src/main/java/com/example/mmg/MainActivity.kt
@@ -8,11 +8,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
-import com.example.mmg.presentation.MmgScreen
 import com.example.mmg.viewmodel.MmgViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mmg.navigation.AppNavigaton
 
@@ -24,32 +20,26 @@ class MainActivity : ComponentActivity(), RobotLifecycleCallbacks {
         super.onCreate(savedInstanceState)
         QiSDK.register(this, this)
 
-        mmgViewModel = MmgViewModel()
-
         setContent {
+            mmgViewModel = viewModel()
+
             val navController = rememberNavController()
             AppNavigaton(navController = navController, mmgViewModel = mmgViewModel)
         }
     }
 
     override fun onRobotFocusGained(qiContext: QiContext?) {
-        // Context für Pepper um seine Funktionen aufrufen zu können
         RoboterActions.qiContext = qiContext
         Log.d("QiContext:", "Focus: ${RoboterActions.qiContext}")
-        // robotExecute gibt an, ob die Roboter Funktionen beim Aufrufen ausgeführt werden sollen
         RoboterActions.robotExecute = true
     }
 
     override fun onRobotFocusLost() {
-        QiSDK.unregister(this,this)
+        QiSDK.unregister(this, this)
         super.onDestroy()
     }
 
     override fun onRobotFocusRefused(reason: String?) {
-
-        // Im Logcat werden Fehlermeldungen ausgeben, falls die Verbindung unterbrochen wird
-        if(reason != null){
-            Log.d("Reason:",reason)
-        }
+        reason?.let { Log.d("Reason:", it) }
     }
 }
