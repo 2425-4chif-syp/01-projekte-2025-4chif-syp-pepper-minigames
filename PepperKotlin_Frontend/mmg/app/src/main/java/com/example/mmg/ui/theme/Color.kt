4c94970cce38927a9848e66c9d0960df566e6516
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-11-03 09:16:49 +0100

    feat: color schemes for composable added

---
+++ PepperKotlin_Frontend/mmg/app/src/main/java/com/example/mmg/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.mmg.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val DarkTeal = Color(0xFF348888)
+    val BrightTeal = Color(0xFF22BABB)
+    val MintGreen = Color(0xFF9EF8EE)
+    val Orange = Color(0xFFFA7F08)
+    val RedOrange = Color(0xFFF24405)
+    
+    // Standard Material Colors
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+}
