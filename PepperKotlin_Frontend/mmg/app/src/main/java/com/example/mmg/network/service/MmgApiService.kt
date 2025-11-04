dda019cff11c365070d16a5b02fd0f50c225d0e5
Nikola Mladenovic <n.mladenovic@students.htl-leonding.ac.at>
2025-11-04 10:12:57 +0100

    fix: fixed false imagor link

--- PepperKotlin_Frontend/mmg/app/src/main/java/com/example/mmg/network/service/MmgApiService.kt
+++ PepperKotlin_Frontend/mmg/app/src/main/java/com/example/mmg/network/service/MmgApiService.kt
@@ -14,6 +14,9 @@ interface MmgApiService {
     @GET("api/tagalongstories/{storyId}/steps")
     suspend fun getSteps(@Path("storyId") storyId: Int): Response<List<StepDto>>
 
-    @GET("api/image/picture/{id}")
+    @GET("imagor/unsafe/fit-in/800x0/http%3A%2F%2Fbackend%3A8080%2Fapi%2Fimage%2Fpicture%2F{id}?ngsw-bypass=true")
     suspend fun getImage(@Path("id") id: Int): Response<ResponseBody>
 }
+
+///imagor/unsafe/fit-in/800x0/http%3A%2F%2Fbackend%3A8080%2Fapi%2Fimage%2Fpicture%2F{id}?ngsw-bypass=true"
+//api/image/picture/{id}
