017b42b1ab5a04d35b890388ebf9ec8f00ec0a8b
DavidCristea797 <d.cristea@students.htl-leonding.ac.at>
2025-12-10 15:54:21 +0100

    fix: changed collection_id

--- pep-caretaker/backend_v2/src/main/java/at/htlleonding/pepper/common/Constants.java
+++ pep-caretaker/backend_v2/src/main/java/at/htlleonding/pepper/common/Constants.java
@@ -1,7 +1,7 @@
 package at.htlleonding.pepper.common;
 
 public class Constants {
-    public static final String COLLECTION_ID = "oldpeople";
+    public static final String COLLECTION_ID = "pepper-user-images";
     public static final String DYNAMODB_TABLE = "facerecognition";
     public static final String FULL_NAME_KEY = "FullName";
     public static final String REKOGNITION_ID_KEY = "RekognitionId";
