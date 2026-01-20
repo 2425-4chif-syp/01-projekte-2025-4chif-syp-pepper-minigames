d0bd9814e95f89ba87e2131e9280969ebdc36655
KyawIT <kyawhtethtun65@gmail.com>
2026-01-20 14:51:09 +0100

    feat(backend): add custom table name

--- pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Picture.java
+++ pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Picture.java
@@ -3,8 +3,10 @@ package com.pep.mealplan.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "pe_picture")
 public class Picture extends PanacheEntity {
 
     @Column(nullable = false)
