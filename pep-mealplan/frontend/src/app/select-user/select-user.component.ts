d3d82b6886dfa4bdc6107969f2d0e4b603f325e2
KyawIT <kyawhtethtun65@gmail.com>
2026-01-21 00:02:01 +0100

    feat(frontend): add new styling

--- pep-mealplan/frontend/src/app/select-user/select-user.component.ts
+++ pep-mealplan/frontend/src/app/select-user/select-user.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { UserAPIService, Resident } from '../residents-api.service';
-import {RouterLink} from "@angular/router";
+import { UserAPIService } from '../services/residents-api.service';
+import { Resident } from '../models/resident.model';
+import { RouterLink } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
     selector: 'app-select-user',
     templateUrl: './select-user.component.html',
     styleUrls: ['./select-user.component.scss'],
-    imports: [
-        RouterLink
-    ],
+    imports: [RouterLink, ButtonModule, AvatarModule],
     standalone: true
 })
 export class SelectUserComponent implements OnInit {
@@ -29,13 +31,17 @@ export class SelectUserComponent implements OnInit {
   }
 
   updateFilteredLetters(): void {
-    const lettersWithUsers = new Set(this.users.map(user => user.Lastname[0].toUpperCase()));
+    const lettersWithUsers = new Set(
+      this.users.map((user) => user.lastname[0].toUpperCase())
+    );
     this.filteredLetters = this.letters.filter(letter => lettersWithUsers.has(letter));
   }
 
   filterByLetter(letter: string): void {
     this.selectedLetter = letter;
-    this.filteredUsers = this.users.filter(user => user.Lastname.startsWith(letter));
+    this.filteredUsers = this.users.filter((user) =>
+      user.lastname.startsWith(letter)
+    );
     this.showNames = true;
   }
 
