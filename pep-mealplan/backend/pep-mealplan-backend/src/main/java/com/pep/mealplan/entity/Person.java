e23d870c4e076b7b8f99a158eebb038b60225a7c
milad <m.moradi@students.htl-leonding.ac.at>
2026-01-23 16:22:47 +0100

    fix: change the person id to person pe.id

--- pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Person.java
+++ pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Person.java
@@ -1,15 +1,12 @@
 package com.pep.mealplan.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
 import java.time.LocalDate;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 @Entity
 @Table(name = "pe_person")
-public class Person extends PanacheEntity {
+public class Person extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,9 +19,6 @@ public class Person extends PanacheEntity {
     @Column(nullable = false)
     public String lastname;
 
-    // optional
     public LocalDate dob;
-
-    // optional
     public String faceId;
 }
