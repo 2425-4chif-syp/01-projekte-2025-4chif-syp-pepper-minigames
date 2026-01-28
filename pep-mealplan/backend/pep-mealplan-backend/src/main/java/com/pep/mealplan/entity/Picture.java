d2e479655f95d3a328621a685df168163787bdea
Kyaw <kyawhtethtun65@gmail.com>
2026-01-28 14:50:02 +0100

    feat(backend): add same image fields as pep-caretaker

--- pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Picture.java
+++ pep-mealplan/backend/pep-mealplan-backend/src/main/java/com/pep/mealplan/entity/Picture.java
@@ -1,24 +1,34 @@
 package com.pep.mealplan.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "pe_picture")
+@Table(name = "pe_image")
 public class Picture extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "i_id")
     public Long id;
 
-    @Column(nullable = false)
-    public String name;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "i_p_id")
+    public Person person;
 
-    @Column(name = "mediatype", nullable = false)
-    public String mediaType;
+    @Column(name = "i_description")
+    public String description;
 
-    @Column(columnDefinition = "TEXT")
-    public String base64;
+    @Column(name = "i_url")
+    public String url;
+
+    @JsonIgnore
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "i_image")
+    public byte[] image;
 
     public Picture() {}
 }
