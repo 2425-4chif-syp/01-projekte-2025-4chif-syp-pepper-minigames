79ce0d7612fc883ca188e3a6d351cd20cd3f61d0
MohamadiAmirhossein <a.mohamadi@students.htl-leonding.ac.at>
2026-02-27 15:15:46 +0100

    feat: updating the Base_url

--- pep-caretaker/PepperKotlin_Frontend/MemoryGamePepper/app/src/main/java/com/example/memorygame/data/remote/NetworkModule.kt
+++ pep-caretaker/PepperKotlin_Frontend/MemoryGamePepper/app/src/main/java/com/example/memorygame/data/remote/NetworkModule.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
-    private const val BASE_URL = "http://vm107.htl-leonding.ac.at:8080/"
+    private const val BASE_URL = "http://vm107.htl-leonding.ac.at"
 
     // private const val BASE_URL = "http://10.0.2.2:8080/" // für Android Emulator (Kein-Pepper-Deployment)
 
